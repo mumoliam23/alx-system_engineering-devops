135c8abd2e45717107126f79474e49926c416318
mumoliam23 <mumoliam23@gmail.com>
2024-03-27 15:32:42 +0300

    Installing a specified version of flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # Installing flask version (2.1.0) using puppet
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
