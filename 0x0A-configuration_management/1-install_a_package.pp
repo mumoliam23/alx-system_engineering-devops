9bfb044d05238ea27dde7f051a44e97e3e8cca10
mumoliam23 <mumoliam23@gmail.com>
2024-03-27 15:42:26 +0300

    Installing a specified version of flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,26 @@
-# Installing flask using puppet
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
-  require => Package['python3-pip'],
+# Define a resource class for managing pip packages
+class pip::package { 
+  $name;
+  $version;
+
+  # Ensure pip3 is available
+  require => Package['python3-pip'];
+
+  # Use pip3 to install the package
+  exec { "install-${name}"
+    command => "/usr/bin/pip3 install ${name}==${version}",
+  }
+
+  # Refresh the package manager cache after installation
+  exec { "update_cache_${name}"
+    command => "/usr/bin/pip3 cache update",
+    refreshonly => true,
+  }
 }
+
+# Install Flask package with version 2.1.0
+pip::package { 
+  name => 'Flask',
+  version => '2.1.0',
+}
+
