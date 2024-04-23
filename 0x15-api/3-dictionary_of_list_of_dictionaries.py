e0c13d946bcd8a4f634463898b2eb341151fe133
mumoliam23 <mumoliam23@gmail.com>
2024-04-23 05:05:48 +0300

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
-
 """
-Python script that exports data in the JSON format.
+returns information about his/her TODO list progress for a
+given employee ID using a REST API and export data in the JSON format
 """
-
-from requests import get
+from sys import argv
 import json
+from collections import OrderedDict
+import requests
+
+
+def get_todo_all():
+    """returns employee's TODO list progress"""
+    r_user = requests.get('https://jsonplaceholder.typicode.com/users')
+
+    try:
+        user_dict = r_user.json()
+        data = OrderedDict()
+        for user in user_dict:
+            data['{}'.format(user["id"])] = []
+            r_todo = requests.get('https://jsonplaceholder.typicode.com/todos?'
+                                  'userId={}'.format(user["id"]))
+            task_list = r_todo.json()
+
+            for task in task_list:
+                a_dict = OrderedDict()
+                a_dict["username"] = user["username"]
+                a_dict["task"] = task["title"]
+                a_dict["completed"] = task["completed"]
+                data['{}'.format(user["id"])].append(a_dict)
+        with open("todo_all_employees.json", "w") as jsonfile:
+            json.dump(data, jsonfile)
+    except:
+        pass
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-
-    row = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-
-    new_dict1 = {}
-
-    for j in data2:
-
-        row = []
-        for i in data:
-
-            new_dict2 = {}
-
-            if j['id'] == i['userId']:
-
-                new_dict2['username'] = j['username']
-                new_dict2['task'] = i['title']
-                new_dict2['completed'] = i['completed']
-                row.append(new_dict2)
-
-        new_dict1[j['id']] = row
-
-    with open("todo_all_employees.json",  "w") as f:
-
-        json_obj = json.dumps(new_dict1)
-        f.write(json_obj)
+    get_todo_all()
