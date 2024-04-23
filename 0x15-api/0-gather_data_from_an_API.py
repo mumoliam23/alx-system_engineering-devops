fb95c851a895d89ad2db8ac8bd13b9b379a54f0d
mumoliam23 <mumoliam23@gmail.com>
2024-04-23 05:00:33 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
-
 """
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+returns information about his/her TODO list progress for a
+given employee ID using a REST API
 """
-
-from requests import get
+import requests
 from sys import argv
 
 
+def get_todo():
+    """returns employee's TODO list progress"""
+    r_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                          .format(argv[1]))
+
+    r_todo_true = requests.get('https://jsonplaceholder.typicode.com/todos?'
+                               'userId={}&&completed=true'.format(argv[1]))
+    r_todo_false = requests.get('https://jsonplaceholder.typicode.com/todos?'
+                                'userId={}&&completed=false'.format(argv[1]))
+    try:
+        user_dict = r_user.json()
+        true_todo_dict = r_todo_true.json()
+        false_todo_dict = r_todo_false.json()
+        print("Employee {} is done with tasks({}/{}):".
+              format(user_dict['name'], len(true_todo_dict),
+                     len(false_todo_dict) + len(true_todo_dict)))
+        for task in true_todo_dict:
+            print("\t {}".format(task['title']))
+    except:
+        pass
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-    completed = 0
-    total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
-
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
-
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
-
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
-                                                          total))
-
-    for i in tasks:
-        print("\t {}".format(i))
+    get_todo()
