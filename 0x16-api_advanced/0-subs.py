4039ac69fe9c3e864e8593da81220e2396f90ab0
mumoliam23 <mumoliam23@gmail.com>
2024-06-05 05:22:28 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """
-Importing request module
+Importing requests module
 """
+
 from requests import get
+
 
 def number_of_subscribers(subreddit):
     """
-    A function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
-    if not subreddit or not isinstance(subreddit, str):
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    all_data = response.json()
 
     try:
-        response = get(url, headers=user_agent, allow_redirects=False)
+        return all_data.get('data').get('subscribers')
 
-        if response.status_code == 200:
-            all_data = response.json()
-            return all_data['data']['subscribers']
-        else:
-            return 0
-    except Exception:
+    except:
         return 0
