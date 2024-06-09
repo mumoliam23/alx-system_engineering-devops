f8b714f2f3af78e31526605edb5adef7f6c38e84
mumoliam23 <mumoliam23@gmail.com>
2024-06-09 15:18:29 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# fix error
-
-exec { 'fix-error':
-  command => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/var/www/html/wp-settings.php',
-}
+# Puppet manifest to fix Apache 500 error
+exec { 'fix-wordpress':
+  command => '/ust/bin/fix-wordpress.sh',
+  path    => ['/bin', '/usr/bin'],
+  }
